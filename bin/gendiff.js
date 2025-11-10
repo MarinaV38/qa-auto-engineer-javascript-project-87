399eae3ee415e27fa4e4ace15e1319f7af4c6263
MarinaV38 <marisabell21@bk.ru>
2025-11-10 12:30:47 +0300

    Аргументы и опции. Шаг 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const { version } = require('../package.json');
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version(version);
+  .version(version)
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
