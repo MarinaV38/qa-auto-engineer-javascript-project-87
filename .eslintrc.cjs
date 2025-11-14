c1879546c35e20ee9551aa4127bdf358af4356c0
MarinaV38 <marisabell21@bk.ru>
2025-11-14 17:32:42 +0300

    fix esLint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,15 @@ module.exports = {
     jest: true,
   },
   extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
     'no-console': 'off',
     'semi': ['error', 'never'],
     'arrow-parens': 'off',
+    'import/extensions': 'off',
+    'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
   },
 }
